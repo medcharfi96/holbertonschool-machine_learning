e6b9b55a97357b2578fe84044bbed6d5ec103404
medcharfi96 <1389@holbertonschool.com>
2020-12-09 23:35:07 +0100

    add

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -7,10 +7,16 @@ des
 def poly_derivative(poly):
     """description"""
     liste = []
+    liste2 = [0]
+    if type(poly) is not list or poly is None:
+        return None
+    for z in range(len(poly)):
+        if poly[z] is not int or poly[z] is not float:
+            return None
     if len(poly) == 0:
         return None
     elif len(poly) == 1:
-        return liste.append(0)
+        return liste2
     else:
         for i in range(1, len(poly)):
             if i == 1 and poly[i] != 0:
