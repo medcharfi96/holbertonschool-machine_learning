4d9026f7c8a9bb3dd4ad7bf9b223377e8cdfa20b
medcharfi96 <1389@holbertonschool.com>
2020-12-09 23:26:47 +0100

    add

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -2,3 +2,21 @@
 """
 des
 """
+
+
+def poly_derivative(poly):
+    """description"""
+    liste = []
+    if len(poly) == 0:
+        return None
+    elif len(poly) == 1:
+        return liste.append(0)
+    else:
+        for i in range(1, len(poly)):
+            if i == 1 and poly[i] != 0:
+                liste.append(poly[i])
+            elif i != 1 and poly[i] == 0:
+                liste.append(0)
+            else:
+                liste.append(poly[i] * i)
+        return liste
