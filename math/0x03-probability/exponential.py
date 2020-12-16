124cbae335b101a2227a6e71c266d88354ba8670
medcharfi96 <1389@holbertonschool.com>
2020-12-16 02:16:10 +0100

    dd

---
+++ math/0x03-probability/exponential.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+second task
+"""
+
+
+class Exponential:
+    """
+    class description
+    """
+
+    e = 2.7182818285
+
+    def __init__(self, data=None, lambtha=1.):
+        """
+        function description
+        :param data: list
+        :param lambtha: value
+        """
+        if data is None:
+            if lambtha < 0:
+                raise ValueError("lambtha must be a positive value")
+            else:
+                self.lambtha = float(lambtha)
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            elif len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = float(len(data) / sum(data))
