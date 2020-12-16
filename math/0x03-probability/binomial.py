596d00b5db5c7cd8e92a5d5ed8868e7768fcba2d
medcharfi96 <1389@holbertonschool.com>
2020-12-16 22:22:47 +0100

    add

---
+++ math/0x03-probability/binomial.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+final task
+"""
+
+
+class Binomial:
+    """
+    class description
+    """
+    e = 2.7182818285
+    pi = 3.1415926536
+
+    def __init__(self, data=None, n=1, p=0.5):
+        """
+        init function
+        :param data: list
+        :param n: int
+        :param p: float
+        """
+        self.data = data
+        if data is None:
+            if n < 0:
+                raise ValueError("n must be a positive value")
+            elif p < 0 or p > 1:
+                raise ValueError(" p must be greater than 0 and less than 1")
+            else:
+                self.n = int(n)
+                self.p = float(p)
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            elif len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            test = 0
+            moy = float(sum(data)/len(data))
+            for i in data:
+                test += pow((i - moy), 2)
+                self.n = int(round(moy / (1 - ((test / len(data)) / moy))))
+                self.p = float(moy / self.n)
