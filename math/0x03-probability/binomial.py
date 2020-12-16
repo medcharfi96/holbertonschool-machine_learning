7dab2ff80f41f23cf43f0b55fa97bf5f7d4eb304
medcharfi96 <1389@holbertonschool.com>
2020-12-16 22:26:17 +0100

    add

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -23,14 +23,14 @@ class Binomial:
             if n <= 0:
                 raise ValueError("n must be a positive value")
             elif p <= 0 or p >= 1:
-                raise ValueError(" p must be greater than 0 and less than 1")
+                raise ValueError("p must be greater than 0 and less than 1")
             else:
                 self.n = int(n)
                 self.p = float(p)
         else:
             if type(data) is not list:
                 raise TypeError("data must be a list")
-            elif len(data) <= 2:
+            elif len(data) < 2:
                 raise ValueError("data must contain multiple values")
             test = 0
             moy = float(sum(data)/len(data))
