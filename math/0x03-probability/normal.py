2e6182a851ed31519a1e8b32016b949530a4bee3
medcharfi96 <1389@holbertonschool.com>
2020-12-16 17:29:59 +0100

    add

---
+++ math/0x03-probability/normal.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+third task
+"""
+
+
+class Normal:
+    """
+    class description
+    """
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """
+        function description l3asba ya wideeed w nik lsbeh
+        :param data: list
+        :param mean: float
+        :param stddev: int
+        """
+        self.data = data
+        if data is None:
+            if stddev < 1:
+                raise ValueError("stddev must be a positive value")
+            else:
+                self.mean = float(mean)
+                self.stddev = float(stddev)
+        else:
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            elif type(data) is not list:
+                raise TypeError("data must be a list")
+            dt = 0
+            self.mean = float(sum(data)/len(data))
+            for i in data:
+                dt += pow((i - self.mean), 2)
+            self.stddev = ((dt/(len(data)))**(0.5))
