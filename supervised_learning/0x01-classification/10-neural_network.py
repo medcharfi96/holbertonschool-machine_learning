4b5c7136eb7851c7ca7272cd45c41db803116842
medcharfi96 <1389@holbertonschool.com>
2021-01-05 13:42:36 +0100

    add

---
+++ supervised_learning/0x01-classification/10-neural_network.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+"""
+classe description
+"""
+import numpy as np
+
+
+class NeuralNetwork():
+    """
+    classe neurone network
+    """
+    def __init__(self, nx, nodes):
+        """
+        constructeur parametré
+        :param nx: int
+        :param nodes: int
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.normal(size=(1, nodes))
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W2(self):
+        """
+        get w2
+        :return:
+        """
+        return self.__W2
+
+    @property
+    def b2(self):
+        """
+        get b2
+        :return:
+        """
+        return self.__b2
+
+    @property
+    def A2(self):
+        """
+        get A2
+        :return:
+        """
+        return self.__A2
+
+    @property
+    def W1(self):
+        """
+        get w1
+        :return:
+        """
+        return self.__W1
+
+    @property
+    def b1(self):
+        """
+        get b1
+        :return:
+        """
+        return self.__b1
+
+    @property
+    def A1(self):
+        """
+        get A1
+        :return:
+        """
+        return self.__A1
+
+    def forward_prop(self, X):
+        """
+        fonction de propagation
+        :param X: ndarray
+        :return:
+        """
+        v1 = np.matmul(self.__W1, X, None) + self.__b1
+        res = 1 / (1 + np.exp(-v1))
+        self.__A1 = res
+        v2 = np.matmul(self.__W2, res, None) + self.__b2
+        res2 = 1 / (1 + np.exp(-v2))
+        self.__A2 = res2
+        return (self.__A1, self.__A2)
