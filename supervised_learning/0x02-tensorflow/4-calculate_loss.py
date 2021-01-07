ee878a5944eecb0fe4ba214a6c6f2fc19a55108b
medcharfi96 <1389@holbertonschool.com>
2021-01-07 15:46:57 +0100

    add

---
+++ supervised_learning/0x02-tensorflow/4-calculate_loss.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" task  """
+import tensorflow as tf
+
+
+def calculate_loss(y, y_pred):
+    """
+    calcule de perte
+    :param y: espace res
+    :param y_pred: le estim
+    """
+    loss = tf.losses.softmax_cross_entropy(y, logits=y_pred, weights=1.0,
+                                           scope=None)
+    return (loss)
