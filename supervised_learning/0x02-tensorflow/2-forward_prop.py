d23c84b7886009b010768762ecf5a0660d6d46a2
medcharfi96 <1389@holbertonschool.com>
2021-01-07 15:29:50 +0100

    add

--- supervised_learning/0x02-tensorflow/2-forward_prop.py
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -11,7 +11,6 @@ def forward_prop(x, layer_sizes=[], activations=[]):
     """
     create_layer = __import__('1-create_layer').create_layer
     hmed = create_layer(x, layer_sizes[0], activations[0])
-    for i in range(len(layer_sizes)):
+    for i in range(1, len(layer_sizes)):
         hmed = create_layer(hmed, layer_sizes[i], activations[i])
-
     return (hmed)
