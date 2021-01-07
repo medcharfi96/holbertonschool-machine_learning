ef818ea2a7fb8eaab8a7edea421afba90f2ab3a6
medcharfi96 <1389@holbertonschool.com>
2021-01-07 23:35:12 +0100

    add

--- supervised_learning/0x01-classification/20-deep_neural_network.py
+++ supervised_learning/0x01-classification/20-deep_neural_network.py
@@ -24,18 +24,18 @@ class DeepNeuralNetwork:
         self.__cache = {}
         self.__weights = {}
         for i in range(len(layers)):
-            if layers[i] <= 0 or not isinstance(layers[i], int):
+            if not isinstance(layers[i], int) or layers[i] <= 0:
                 raise TypeError("layers must be a list of positive integers")
-            wb = "b{}" + str(i + 1)
-            wK = "W{}" + str(i + 1)
+
             if i > 0:
-                fr = np.sqrt(2 / layers[i - 1])
-                self.weights[wK] = np.random.randn(layers[i],
-                                                   layers[i - 1]) * fr
+                self.__weights['W' + str(i + 1)] = \
+                    np.random.randn(layers[i], layers[i - 1]) * \
+                    np.sqrt(2 / layers[i - 1])
             else:
-                fr = np.sqrt(2 / nx)
-                self.weights[wK] = np.random.randn(layers[0], nx) * fr
-                self.weights[wb] = np.zeros((layers[0], 1))
+                v = np.sqrt(2 / nx)
+                self.__weights['W' + str(i + 1)
+                               ] = np.random.randn(layers[0], nx) * v
+            self.__weights['b' + str(i + 1)] = np.zeros((layers[i], 1))
 
     @property
     def L(self):
@@ -58,14 +58,14 @@ class DeepNeuralNetwork:
         :param X: int
         :return:
         """
+
         self.__cache["A0"] = X
         for i in range(self.__L):
             wb = "b" + str(i+1)
             wk = "W" + str(i+1)
             wa = "A" + str(i)
             wa2 = "A" + str(i+1)
-            v = np.matmul(self.__weights[wk], self.__cache[wa], None) +\
-                self.__weights[wb]
+            v = np.matmul(self.__weights[wk], self.__cache[wa]) + self.__weights[wb]
             res = 1 / (1 + np.exp(-v))
             self.__cache[wa2] = res
         return (self.__cache[wa2], self.__cache)
@@ -90,7 +90,7 @@ class DeepNeuralNetwork:
         :return:
         """
 
-        z = self.forward_prop(X)
+        z, khringa = self.forward_prop(X)
         ras = np.round(z)
         lal = self.cost(Y, z)
         return (ras.astype(np.int), lal)
