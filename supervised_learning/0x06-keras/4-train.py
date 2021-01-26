be2ed9491b354023c5622c8f30299c307155b1a1
medcharfi96 <1389@holbertonschool.com>
2021-01-26 21:02:20 +0100

    addd

---
+++ supervised_learning/0x06-keras/4-train.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" task 4"""
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                verbose=True, shuffle=False):
+    """
+    des
+    :param network:
+    :param data:
+    :param labels:
+    :param batch_size:
+    :param epochs:
+    :param verbose:
+    :param shuffle:
+    """
+    history = network.fit(x=data, y=labels,
+                          epochs=epochs,
+                          batch_size=batch_size,
+                          shuffle=shuffle,
+                          verbose=verbose)
+    return history
