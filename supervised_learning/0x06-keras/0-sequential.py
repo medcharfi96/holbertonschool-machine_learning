7e6020fd54dd47a541ddacd0d910fbd21a4cafe6
medcharfi96 <1389@holbertonschool.com>
2021-01-26 20:34:45 +0100

    addd

---
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" task 0"""
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """
+    classe neuronal
+    :param nx: int
+    :param layers: list
+    :param activations: list
+    :param lambtha: int
+    :param keep_prob: prob int
+    """
+    model = K.Sequential()
+    krnl = K.regularizers.l2(lambtha)
+    model.add(K.layers.Dense(layers[0], activation=activations[0],
+                             input_dim=nx, kernel_regularizer=krnl,
+                             name='dense'))
+    for i in range(1, len(layers)):
+        model.add(K.layers.Dense(layers[i], activation=activations[i],
+                                 kernel_regularizer=krnl,
+                                 name='dense'+str(i)))
+        if i < len(layers) - 1:
+            model.add(K.layers.Dropout(1-keep_prob))
+    return model
