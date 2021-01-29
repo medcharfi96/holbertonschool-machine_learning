04a3e26802555f34285d21571cfb01fb7c614c87
medcharfi96 <1389@holbertonschool.com>
2021-01-29 18:48:50 +0100

    ad

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -18,9 +18,9 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
                              input_dim=nx, kernel_regularizer=krnl,
                              name='dense'))
     for i in range(1, len(layers)):
+        model.add(K.layers.Dropout(1 - keep_prob))
         model.add(K.layers.Dense(layers[i], activation=activations[i],
                                  kernel_regularizer=krnl,
                                  name='dense'+str(i)))
-        if i < len(layers) - 1:
-            model.add(K.layers.Dropout(1-keep_prob))
+
     return model
