5ce10f645ae9500da74a245d54becaa65c3ac592
medcharfi96 <1389@holbertonschool.com>
2021-01-29 19:07:04 +0100

    ad

---
+++ supervised_learning/0x06-keras/8-train.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+""" task 6 """
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels,
+                batch_size, epochs,
+                validation_data=None,
+                early_stopping=False,
+                patience=0, learning_rate_decay=False,
+                alpha=0.1, decay_rate=1,
+                save_best=False, filepath=None,
+                verbose=True,
+                shuffle=False):
+    """
+    zeb
+    :param network:
+    :param data:
+    :param labels:
+    :param batch_size:
+    :param epochs:
+    :param validation_data:
+    :param early_stopping:
+    :param patience:
+    :param verbose:
+    :param shuffle:
+    """
+    def scheduler(epoch):
+        """
+        ay 3asba
+        :param epoch:
+        """
+        return alpha / (1 + decay_rate * epoch)
+    test = []
+    if early_stopping is True:
+        wakaf = K.callbacks.EarlyStopping(monitor='val_loss',
+                                          patience=patience)
+        test.append(wakaf)
+    if learning_rate_decay and validation_data:
+        zab = K.callbacks.LearningRateScheduler(scheduler,
+                                                verbose=1)
+        test.append(zab)
+    if save_best is True:
+        zbib = K.callbacks.ModelCheckpoint(filepath)
+        test.append(zbib)
+    history = network.fit(x=data, y=labels,
+                          callbacks=test,
+                          epochs=epochs,
+                          batch_size=batch_size,
+                          validation_data=validation_data,
+                          verbose=verbose,
+                          shuffle=shuffle)
+    return history
