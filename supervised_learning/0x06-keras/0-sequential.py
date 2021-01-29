e55cc558373d761c8cd72dc250394e20701b0257
medcharfi96 <1389@holbertonschool.com>
2021-01-29 18:50:55 +0100

    ad

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -21,6 +21,6 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
         model.add(K.layers.Dropout(1 - keep_prob))
         model.add(K.layers.Dense(layers[i], activation=activations[i],
                                  kernel_regularizer=krnl,
-                                 name='dense'+str(i)))
+                                 name='dense_'+str(i)))
 
     return model
