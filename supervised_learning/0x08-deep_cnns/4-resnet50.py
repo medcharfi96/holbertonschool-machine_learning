d412c58d504c21de921e6bfda58ace4c46845522
medcharfi96 <1389@holbertonschool.com>
2021-02-11 13:39:53 +0100

    add

--- supervised_learning/0x08-deep_cnns/4-resnet50.py
+++ supervised_learning/0x08-deep_cnns/4-resnet50.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ task 4 """
-import tensorflow._api.v1.keras as K
+import tensorflow.keras as K
 identity_block = __import__('2-identity_block').identity_block
 projection_block = __import__('3-projection_block').projection_block
 
