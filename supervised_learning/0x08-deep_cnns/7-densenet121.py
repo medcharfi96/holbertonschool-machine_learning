898358474e2e0a71fbdc5b22fbe5fd2641fc6a67
medcharfi96 <1389@holbertonschool.com>
2021-02-14 15:31:47 +0100

    add

--- supervised_learning/0x08-deep_cnns/7-densenet121.py
+++ supervised_learning/0x08-deep_cnns/7-densenet121.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ task 7"""
-import tensorflow._api.v1.keras as K
+import tensorflow.keras as K
 dense_block = __import__('5-dense_block').dense_block
 transition_layer = __import__('6-transition_layer').transition_layer
 
