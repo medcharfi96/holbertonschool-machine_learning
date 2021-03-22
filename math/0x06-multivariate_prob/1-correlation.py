b5df9b766700eaaee303fc12237dfe7d7d21a8c6
medcharfi96 <1389@holbertonschool.com>
2021-03-22 15:07:35 +0100

    add

---
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" task 1 """
+
+import numpy as np
+
+
+def correlation(C):
+    """
+    desc
+    :param C:
+    """
+    if C != np.ndarray:
+        raise TypeError('C must be a numpy.ndarray')
+
+    if len(C.shape) != 2:
+        raise ValueError("C must be a 2D square matrix")
+
+    if (C.shape[0] != C.shape[1]):
+        raise ValueError("C must be a 2D square matrix")
+
+    variance = np.sqrt(np.diag(C))
+    cor = 1 / (np.outer(variance, variance) / C)
+    return cor
