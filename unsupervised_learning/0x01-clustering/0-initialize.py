d62315da6bae266230b9b18e4c904b7a4f722032
medcharfi96 <1389@holbertonschool.com>
2021-03-31 16:12:00 +0100

    add

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -14,6 +14,8 @@ def initialize(X, k):
     n, d = X.shape
     if type(k) is not int or k <= 0:
         return None
+    if type(X) != np.ndarray or len(X.shape) != 2:
+        return None
     min = X.min(axis=0)
     max = X.max(axis=0)
     point = np.random.uniform(low=min, high=max, size=(k, d))
