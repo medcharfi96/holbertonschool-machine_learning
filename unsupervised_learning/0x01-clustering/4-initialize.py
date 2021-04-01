c81014e82660906273be6b57733a7b20aba58a89
medcharfi96 <1389@holbertonschool.com>
2021-04-01 14:25:56 +0100

    add

---
+++ unsupervised_learning/0x01-clustering/4-initialize.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" task 4 """
+
+
+import numpy as np
+kmeans = __import__('1-kmeans').kmeans
+
+
+def initialize(X, k):
+    """
+    initialisation de variable de gaussian
+    :param X:
+    :param k:
+    """
+
+    if type(X) != np.ndarray or len(X.shape) != 2:
+        return None, None, None
+
+    if type(k) != int or k <= 0:
+        return None, None, None
+
+    n, d = X.shape
+    pi = np.tile(1/k, (k))
+    c, clss = kmeans(X, k)
+    S = np.tile(np.identity(d), (k, d, d))
+
+    return pi, c, S
