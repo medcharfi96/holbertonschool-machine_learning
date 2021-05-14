ef9f81c4fcdda69d18a3b0db596c57b8735add81
medcharfi96 <1389@holbertonschool.com>
2021-05-14 22:26:39 +0100

    add

---
+++ supervised_learning/0x10-nlp_metrics/2-cumulative_bleu.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+""" task 2 """
+
+
+import numpy as np
+
+
+def cumulative_bleu(references, sentence, n):
+    """
+    calcule de  n  gram
+    :param references:list
+    :param sentence:list
+    :param n: int
+    """
+    referenc_l = []
+    flat = {}
+
+    sent = [' '.join([str(ze) for ze in sentence[id:id + n]])
+            for id in range(len(sentence) - (n - 1))]
+    N_out_sent = (len(sent))
+
+    for refs in references:
+        refera_N = [' '.join([str(ze) for ze in refs[id:id + n]])
+                    for id in range(len(sentence) - (n - 1))]
+
+        referenc_l.append(len(refs))
+
+        for word in refera_N:
+            if word in sent:
+                if not flat.keys() == word:
+                    flat[word] = 1
+
+    flat_sum = sum(flat.values())
+    idx_proche = np.argmin([abs(len(x) - N_out_sent) for x in references])
+    idx_proche_l = len(references[idx_proche])
+
+    if N_out_sent < idx_proche_l:
+        bp = np.exp(1 - (idx_proche_l / len(sentence)))
+    else:
+        bp = 1
+    return bp * np.exp(np.sum((1 / n) * np.log(flat_sum / N_out_sent)))
