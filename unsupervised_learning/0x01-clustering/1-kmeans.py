3b0a30b4aa265da8cb03c41ddc115b53b0573ce7
medcharfi96 <1389@holbertonschool.com>
2021-06-11 00:08:18 +0100

    add

--- unsupervised_learning/0x01-clustering/1-kmeans.py
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -7,50 +7,60 @@ import numpy as np
 
 def kmeans(X, k, iterations=1000):
     """
-    faire le test des kmeans
+    k mean
     :param X:
     :param k:
     :param iterations:
+    :return:
     """
-    if type(X) is not np.ndarray or len(X.shape) != 2:
+
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None
-    if type(k) is not int or k <= 0:
+
+    if type(k) != int or k <= 0:
         return None, None
+
+    if type(iterations) != int or iterations <= 0:
+        return None, None
+
     n, d = X.shape
-    if n < k:
-        return None, None
+
+    minimum = np.amin(X, axis=0)
+    maximum = np.amax(X, axis=0)
+
     C = initialize(X, k)
-    ancien = np.ndarray.copy(C)
+    clss = None
     for i in range(iterations):
-        delta = X - C[:, np.newaxis]
-        dsit = np.sqrt((delta ** 2).sum(axis=2))
-        clss = np.argmin(dsit, axis=0)
+        C_cpy = np.copy(C)
+        distance = np.linalg.norm(X[:, None] - C, axis=-1)
+        clss = np.argmin(distance, axis=-1)
         for j in range(k):
-            if len(X[clss == j]) == 0:
+            index = np.argwhere(clss == j)
+            if not len(index):
                 C[j] = initialize(X, 1)
             else:
-                C[j] = np.mean(X[clss == j], axis=0)
-        delta = X - C[:, np.newaxis]
-        dsit = np.sqrt((delta ** 2).sum(axis=2))
-        clss = np.argmin(dsit, axis=0)
-        if np.all(C == ancien):
+                C[j] = np.mean(X[index], axis=0)
+
+        if (C_cpy == C).all():
             return C, clss
+
+    distance = np.linalg.norm(X[:, None] - C, axis=-1)
+    clss = np.argmin(distance, axis=-1)
+
     return C, clss
 
 
 def initialize(X, k):
     """
-    inisialisation de cluster
+    init
     :param X:
     :param k:
+    :return:
     """
-    if type(X) != np.ndarray or len(X.shape) != 2:
-        return None
-    if type(k) is not int or k <= 0:
-        return None
+
     n, d = X.shape
 
-    min = X.min(axis=0)
-    max = X.max(axis=0)
-    point = np.random.uniform(low=min, high=max, size=(k, d))
-    return point
+    minimum = np.amin(X, axis=0)
+    maximum = np.amax(X, axis=0)
+    values = np.random.uniform(minimum, maximum, (k, d))
+    return values
