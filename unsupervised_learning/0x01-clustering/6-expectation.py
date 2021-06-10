612ef80d8bd0d50e393ccd006f980f09b64160b6
medcharfi96 <1389@holbertonschool.com>
2021-06-10 23:48:27 +0100

    add

--- unsupervised_learning/0x01-clustering/6-expectation.py
+++ unsupervised_learning/0x01-clustering/6-expectation.py
@@ -15,18 +15,23 @@ def expectation(X, pi, m, S):
     :param S:
     :return:
     """
-    if type(X) != np.ndarray or len(X.shape) != 2:
-        return None
-    if type(m) != np.ndarray or len(m.shape) != 1:
-        return None
-    if type(S) != np.ndarray or len(S.shape) != 2:
-        return None
-    if X.shape[1] != m.shape[0]:
-        return None
-    if X.shape[1] != S.shape[0] or X.shape[1] != S.shape[1]:
-        return None
-    if S.shape[0] != S.shape[1]:
-        return None
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None, None
+    if type(m) is not np.ndarray or len(m.shape) != 2:
+        return None, None
+    if type(S) is not np.ndarray or len(S.shape) != 3:
+        return None, None
+    if type(pi) is not np.ndarray or len(pi.shape) != 1:
+        return None, None
+    if X.shape[1] != S.shape[1] or S.shape[1] != S.shape[2]:
+        return (None, None)
+    if X.shape[1] != m.shape[1] or m.shape[0] != S.shape[0]:
+        return (None, None)
+    if pi.shape[0] != m.shape[0]:
+        return (None, None)
+    if not np.isclose(np.sum(pi), 1):
+        return None, None
+
     n, d = X.shape
     k = S.shape[0]
     tmp = np.zeros((k, n))
