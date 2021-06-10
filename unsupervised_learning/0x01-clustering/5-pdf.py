5db337eef3ac1e9fb295b03aaee146218e11d0f2
medcharfi96 <1389@holbertonschool.com>
2021-06-10 23:43:13 +0100

    add

--- unsupervised_learning/0x01-clustering/5-pdf.py
+++ unsupervised_learning/0x01-clustering/5-pdf.py
@@ -24,5 +24,13 @@ def pdf(X, m, S):
         return None
     if S.shape[0] != S.shape[1]:
         return None
-
     n, d = X.shape
+    x_m = X - m
+    S_inv = np.linalg.inv(S)
+
+    fac = np.einsum('...k,kl,...l->...', x_m, S_inv, x_m)
+    P1 = 1. / (np.sqrt(((2 * np.pi) ** d * np.linalg.det(S))))
+    P2 = np.exp(-fac / 2)
+    P = np.maximum((P1 * P2), 1e-300)
+
+    return P
