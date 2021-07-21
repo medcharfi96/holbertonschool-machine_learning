599a9c88b5ae62a1dba510c3c47d30f21d3fd1d6
medcharfi96 <1389@holbertonschool.com>
2021-07-21 20:42:59 +0100

    add

--- pipeline/0x00-pandas/11-concat.py
+++ pipeline/0x00-pandas/11-concat.py
@@ -3,16 +3,17 @@
 import pandas as pd
 from_file = __import__('2-from_file').from_file
 
-zeb1 = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
-zeb2 = from_file('bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv', ',')
+df1 = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+df2 = from_file('bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv', ',')
 
 """
-dsc
+desc
 """
+df1 = df1.set_index('Timestamp')
+df2 = df2.set_index('Timestamp')
 
-zeb1 = zeb1.set_index('Timestamp')
-zeb2 = zeb2.set_index('Timestamp')
-zeb2 = zeb2.loc[:'1417411920']
-zeb = pd.concat([zeb2, zeb1], keys=['bitstamp', 'coinbase'])
+df2 = df2.loc[:'1417411920']
 
-print(zeb)
+df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
+
+print(df)
--- pipeline/0x00-pandas/12-hierarchy.py
+++ pipeline/0x00-pandas/12-hierarchy.py
@@ -5,17 +5,19 @@ Hierarchy script
 import pandas as pd
 from_file = __import__('2-from_file').from_file
 
-zeb1 = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
-zeb2 = from_file('bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv', ',')
+df1 = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+df2 = from_file('bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv', ',')
 
-"""desc"""
-zeb1 = zeb1.set_index('Timestamp')
-zeb2 = zeb2.set_index('Timestamp')
+"""
+desc
+"""
+df1 = df1.set_index('Timestamp')
+df2 = df2.set_index('Timestamp')
 
-zeb1 = zeb1.loc['1417411980':'1417417980']
-zeb2 = zeb2.loc['1417411980':'1417417980']
+df1 = df1.loc['1417411980':'1417417980']
+df2 = df2.loc['1417411980':'1417417980']
 
-df = pd.concat([zeb2, zeb1], keys=['bitstamp', 'coinbase'])
+df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
 df = df.swaplevel(i=0, j=1, axis=0).sort_index()
 
 print(df)
