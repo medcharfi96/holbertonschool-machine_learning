95ba5fb6fcf34ecf9f886a85d293ca42aec9af5c
medcharfi96 <1389@holbertonschool.com>
2021-07-21 20:27:52 +0100

    add

---
+++ pipeline/0x00-pandas/5-slice.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" task 5 """
+import pandas as pd
+from_file = __import__('2-from_file').from_file
+
+df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+
+"""
+desc
+"""
+df = df.loc[::60, ['High', 'Low', 'Close', 'Volume_(BTC)']]
+
+print(df.tail())
