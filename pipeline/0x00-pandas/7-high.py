b22ee52d5793d0460075ed5452f3387fbc78c4af
medcharfi96 <1389@holbertonschool.com>
2021-07-21 20:30:27 +0100

    add

---
+++ pipeline/0x00-pandas/7-high.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" task 7 """
+
+import pandas as pd
+from_file = __import__('2-from_file').from_file
+
+df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+
+"""
+dsc
+"""
+df = df.sort_values(by='High', ascending=False)
+print(df.head())
