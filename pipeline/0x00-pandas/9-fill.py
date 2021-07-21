68d33565f2de993b9564a6d1c50637c6c0706d06
medcharfi96 <1389@holbertonschool.com>
2021-07-21 20:33:11 +0100

    add

---
+++ pipeline/0x00-pandas/9-fill.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" task 10 """
+import pandas as pd
+from_file = __import__('2-from_file').from_file
+
+df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+
+# YOUR CODE HERE
+"""
+desc
+"""
+df = df.drop(columns=['Weighted_Price'])
+cols = ['High', 'Low', 'Open']
+df['Close'].fillna(method='ffill', inplace=True)
+
+for i in cols:
+    df[i].fillna(value=df.Close.shift(1, axis=0), inplace=True)
+df['Volume_(BTC)'].fillna(value=0, inplace=True)
+df['Volume_(Currency)'].fillna(value=0, inplace=True)
+print(df.head())
+print(df.tail())
