13342de2b08a72bc1d68019523e8572efa76be15
medcharfi96 <1389@holbertonschool.com>
2021-07-21 20:41:13 +0100

    add

---
+++ pipeline/0x00-pandas/13-analyze.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""task 13 """
+import pandas as pd
+from_file = __import__('2-from_file').from_file
+
+df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+
+"""
+desc
+"""
+stats = df.drop(['Timestamp'], axis=1).describe()
+
+print(stats)
