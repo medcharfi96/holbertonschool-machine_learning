8a0434fed7cbbad40007d18fcc38f7e2db15b0a4
medcharfi96 <1389@holbertonschool.com>
2021-07-26 23:55:46 +0100

    add

---
+++ pipeline/0x01-apis/3-upcoming.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""  task 3 """
+import requests
+
+
+if __name__ == '__main__':
+    """
+    give lunch info
+    """
+    ADRESS = "https://api.spacexdata.com/v4/launches/upcoming"
+    RES = requests.get(ADRESS)
+    data = RES.json()
+    data.sort(key=lambda json: json['date_unix'])
+    data = data[0]
+
+    F_NOM = data["name"]
+
+    F_tmp_LOC = data["date_local"]
+
+    # <rocket name>
+    ADRESS2 = "https://api.spacexdata.com/v4/rockets/" + data["rocket"]
+    F_NOM_ROCK = requests.get(ADRESS2).json()['name']
+
+    # <launchpad name> (<launchpad locality>)
+    ADRESS3 = "https://api.spacexdata.com/v4/launchpads/" +\
+        data["launchpad"]
+    REQUTTE = requests.get(ADRESS3).json()
+    F_NOM_LUNCH = REQUTTE['name']
+    F_LUNCH_LOC = REQUTTE['locality']
+
+    print("{} ({}) {} - {} ({})".format(F_NOM, F_tmp_LOC,
+                                        F_NOM_ROCK, F_NOM_LUNCH,
+                                        F_LUNCH_LOC))
